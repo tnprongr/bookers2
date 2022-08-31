168d4d70ae49ab4f567205265f2dcf38df073c6c
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-08-31 23:31:28 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+
+  resources :books, only: [:show, :index, :edit ,:destroy]
+  resources :users, only: [:show, :index, :edit]
+
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
