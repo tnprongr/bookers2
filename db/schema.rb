34c86e07d11eb13c2b5131bfd728c50d77a6b610
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-09-01 01:20:16 +0000

    [Add]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_115912) do
+ActiveRecord::Schema.define(version: 2022_09_01_001707) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
