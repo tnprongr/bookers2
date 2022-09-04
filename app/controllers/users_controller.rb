11011ed71e7c15f921e2b014c880ba0254f0ea41
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-09-04 12:40:16 +0000

    [Add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
   def show
     @user = User.new
-    
   end
 
   def index
+    @users = User.all
   end
 
   def edit
+    
   end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)  
+  end
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,20 @@
 class Book < ApplicationRecord
   belongs_to :user
+  
+  def new
+    @book = Book.new  
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
