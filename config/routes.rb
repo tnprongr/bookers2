7eac17978b75eaa3be2bc62b5908e3f5073aa5b2
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-09-16 05:30:59 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:show, :index, :create, :edit ,:destroy]
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :create] 
 
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
