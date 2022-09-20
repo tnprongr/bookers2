08c05faba79311f39c59b8bff16ab933d3d6cd1e
tnprongr <106970246+tnprongr@users.noreply.github.com>
2022-09-20 14:10:49 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,33 @@
 class BooksController < ApplicationController
-  def show
+
+  def new
+   @book = Book.new
   end
 
   def index
    @books = Book.all
+   @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    if @book.save
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  private
 
   def book_params
     params.require(:book).permit(:title, :body)
